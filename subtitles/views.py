5a80e98f243a3c0faf82741d24647cdcae460c72
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2016-11-30 23:09:22 -0500

    - se comprueba los mimetypes de los ficheros que se suben para que no se indicen ficheros qeu no sean de texto ni se traten de descomprimir ficheros que no sean zip - comentado temporalmente el manejo de errores en upload

--- subtitles/views.py
+++ subtitles/views.py
@@ -1,10 +1,11 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 from __future__ import unicode_literals
 from os import remove, mkdir
 
 import operator
 from rexec import FileWrapper
 
+import magic
 from django.contrib import messages
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
@@ -24,7 +25,7 @@ from subtitles.models import Subtitulo
 from subtitles.util import id_generator
 
 EXTENSIONES = ['.srt', '.sub', '.ass', '.zip']
-
+mime = magic.Magic(mime=True)
 
 def index(request):
     return render(request, 'index.html')
@@ -35,7 +36,7 @@ def search(request):
 
     if request.GET.has_key('q') and request.GET['q'] != '':
         q = request.GET['q']
-        paginator = Paginator(search_keywords(Subtitulo,q.split()), 10)
+        paginator = Paginator(search_keywords(Subtitulo, q.split()), 10)
         # paginator = Paginator(Subtitulo.objects.filter(nombre__search=q), 10)
         page = request.GET.get('page')
 
@@ -48,15 +49,15 @@ def search(request):
     else:
         return HttpResponseRedirect('/')
 
-    return render(request, 'search.html', {'subs':subs, 'search_term':q})
+    return render(request, 'search.html', {'subs': subs, 'search_term': q})
 
 
 def upload(request):
     if request.method == 'POST':
-        try:
+        #try:
             if request.FILES.has_key('afile'):
                 afile = request.FILES['afile']
-                fname,ext = splitext(afile.name)
+                fname, ext = splitext(afile.name)
                 rand_name = id_generator()
                 path = TMP_FOLDER + rand_name
 
@@ -66,65 +67,79 @@ def upload(request):
 
                 if ext in EXTENSIONES:
                     if ext == '.zip':
-                        # archive = zipfile.ZipFile(path)
-                        aux_ext_path = TMP_FOLDER+rand_name+'_ext'
-                        mkdir(aux_ext_path)
-                        unzip_all(path, aux_ext_path)
-                        # archive.extractall(aux_ext_path)
-                        remove(path)
+                        if mime.from_file(path) == 'application/zip':
+                            # archive = zipfile.ZipFile(path)
+                            aux_ext_path = TMP_FOLDER + rand_name + '_ext'
+                            mkdir(aux_ext_path)
+                            unzip_all(path, aux_ext_path)
+                            # archive.extractall(aux_ext_path)
+                            remove(path)
 
-                        matches = []
-                        for root, dirnames, filenames in os.walk(aux_ext_path):
-                            for filename in fnmatch.filter(filenames, '*.srt'):
-                                matches.append(os.path.join(root, filename))
-                            for filename in fnmatch.filter(filenames, '*.sub'):
-                                matches.append(os.path.join(root, filename))
-                            for filename in fnmatch.filter(filenames, '*.ass'):
-                                matches.append(os.path.join(root, filename))
-                        count = 0
-                        for i in matches:
-                            d = open(i, "rb").read()
+                            matches = []
+                            for root, dirnames, filenames in os.walk(aux_ext_path):
+                                for filename in fnmatch.filter(filenames, '*.srt'):
+                                    matches.append(os.path.join(root, filename))
+                                for filename in fnmatch.filter(filenames, '*.sub'):
+                                    matches.append(os.path.join(root, filename))
+                                for filename in fnmatch.filter(filenames, '*.ass'):
+                                    matches.append(os.path.join(root, filename))
+                            count = 0
+                            for i in matches:
+                                d = open(i, "rb").read()
+                                thash = sha256(d)
+                                sha = thash.hexdigest()
+
+                                if mime.from_file(i) == 'text/plain':
+                                    if len(Subtitulo.objects.filter(ahash=sha)) == 0:
+                                        copy(i, STASH_FOLDER + sha)
+                                        Subtitulo(nombre=unidecode(basename(i)), ruta=sha, ahash=sha).save()
+                                        count += 1
+
+                            messages.add_message(request, messages.SUCCESS,
+                                                 "Se han agregado " + str(count) + " subtítulos nuevos")
+                            if count < len(matches):
+                                messages.add_message(request, messages.INFO, "Se omitieron " + str(
+                                    len(matches) - count) + " subtítulos que ya estaban en la colección")
+
+                            rmtree(TMP_FOLDER + rand_name + '_ext')
+                        else:
+                            messages.add_message(request, messages.ERROR,
+                                                 "Los tipos de fichero permitidos son .srt | .sub | .ass | .zip")
+                    else:
+
+                        d = open(path, "rb").read()
+
+                        if mime.from_file(path) == 'text/plain':
                             thash = sha256(d)
                             sha = thash.hexdigest()
 
-                            if len(Subtitulo.objects.filter(ahash=sha)) == 0:
-                                copy(i, STASH_FOLDER + sha)
-                                Subtitulo(nombre=unidecode(basename(i)), ruta=sha, ahash=sha).save()
-                                count += 1
-
-                        messages.add_message(request, messages.SUCCESS, "Se han agregado "+str(count)+" subtítulos nuevos")
-                        if count < len(matches):
-                            messages.add_message(request, messages.INFO, "Se omitieron "+str(len(matches)-count)+" subtítulos que ya estaban en la colección")
-
-                        rmtree(TMP_FOLDER+rand_name+'_ext')
-                    else:
-                        d = open(path, "rb").read()
-                        thash = sha256(d)
-                        sha = thash.hexdigest()
-
-                        if len(Subtitulo.objects.filter(ahash=sha)) > 0 :
-                            messages.add_message(request, messages.WARNING,"Ya tenemos este subtítulo. Gracias por compartir")
+                            if len(Subtitulo.objects.filter(ahash=sha)) > 0:
+                                messages.add_message(request, messages.WARNING,
+                                                     "Ya tenemos este subtítulo. Gracias por compartir")
+                            else:
+                                copy(path, STASH_FOLDER + sha)
+                                remove(path)
+                                Subtitulo(nombre=unidecode(afile.name), ruta=sha, ahash=sha).save()
+                                messages.add_message(request, messages.SUCCESS, "Subtítulo agregado correctamente")
                         else:
-                            copy(path,STASH_FOLDER+sha)
-                            remove(path)
-                            Subtitulo(nombre=unidecode(afile.name), ruta=sha, ahash=sha).save()
-                            messages.add_message(request, messages.SUCCESS, "Subtítulo agregado correctamente")
+                            messages.add_message(request, messages.ERROR,
+                                                 "Los tipos de fichero permitidos son .srt | .sub | .ass | .zip")
                 else:
-                    messages.add_message(request, messages.ERROR, "Los tipos de fichero permitidos son .srt | .sub | .ass | .zip")
+                    messages.add_message(request, messages.ERROR,
+                                         "Los tipos de fichero permitidos son .srt | .sub | .ass | .zip")
             else:
                 messages.add_message(request, messages.ERROR, "No ha incluido ningún fichero")
-        except:
-            messages.add_message(request, messages.ERROR, "Ha ocurrido un error mientras se procesaba el fichero")
-
+        #except:
+        #    messages.add_message(request, messages.ERROR, "Ha ocurrido un error mientras se procesaba el fichero")
 
     return render(request, 'upload.html')
 
 
-def download(request,id):
+def download(request, id):
     subtitulo = Subtitulo.objects.get(id=id)
     subtitulo.descargas += 1
     subtitulo.save()
-    response = HttpResponse(FileWrapper(open(STASH_FOLDER+subtitulo.ruta, 'rb')),
+    response = HttpResponse(FileWrapper(open(STASH_FOLDER + subtitulo.ruta, 'rb')),
                             content_type='text/plain')
     nombre = '"' + subtitulo.nombre + '"'
 
@@ -141,7 +156,7 @@ def search_keywords(subs, keywords):
 
     nameSearch = [Q(nombre__icontains=x) for x in keywords]
 
-    r_qs = subs.objects.filter(reduce(operator.and_,nameSearch)).order_by("-descargas","nombre")
+    r_qs = subs.objects.filter(reduce(operator.and_, nameSearch)).order_by("-descargas", "nombre")
     return r_qs
 
 
@@ -149,36 +164,44 @@ def unzip_all(azipfile, ext_path):
     zf = zipfile.ZipFile(azipfile, 'r')
     for m in zf.infolist():
         data = zf.read(m)
-        #this is not a solution
-        #Fix this properly
-        try:
-            print ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii', 'ignore')
-            disk_file_name = ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii',
-                                                                                                        'ignore')
-        except:
-            print 'unicode error'
+        # this is not a solution
+        # Fix this properly
+
+        disk_file_name = ''
 
         try:
-            print ext_path + '/' + m.filename.decode('unicode_escape').encode('ascii', 'ignore')
-            disk_file_name = ext_path + '/' + m.filename.decode('unicode_escape').encode('ascii', 'ignore')
+            print ext_path + '/' + unidecode(m.filename)
+            disk_file_name = ext_path + '/' + unidecode(m.filename)
         except:
-            print 'unicode error 2'
+            print 'unidecode failed'
+            try:
+                print ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii', 'ignore')
+                disk_file_name = ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii','ignore')
+            except:
+                print 'unicode error on method 1'
+
+                try:
+                    print ext_path + '/' + m.filename.decode('unicode_escape').encode('ascii', 'ignore')
+                    disk_file_name = ext_path + '/' + m.filename.decode('unicode_escape').encode('ascii', 'ignore')
+                except:
+                    print 'unicode error on method 2'
 
         # disk_file_name = ext_path+'/'+ m.filename.decode('unicode_escape').encode('ascii','ignore')
-        #disk_file_name = ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii', 'ignore')
-        dir_name = os.path.dirname(disk_file_name)
-        try:
-            os.makedirs(dir_name)
-        except OSError as e:
-            if e.errno == os.errno.EEXIST:
-                pass
-            else:
+        # disk_file_name = ext_path + '/' + unicodedata.normalize('NFKD', unicode(m.filename)).encode('ascii', 'ignore')
+        if disk_file_name != '':
+            dir_name = os.path.dirname(disk_file_name)
+            try:
+                os.makedirs(dir_name)
+            except OSError as e:
+                if e.errno == os.errno.EEXIST:
+                    pass
+                else:
+                    raise
+            except Exception as e:
                 raise
-        except Exception as e:
-            raise
-        try:
-            with open(disk_file_name, 'wb') as fd:
-                fd.write(data)
-        except:
-            pass
+            try:
+                with open(disk_file_name, 'wb') as fd:
+                    fd.write(data)
+            except:
+                pass
     zf.close()
